a2e151509b66eeae0af0b348feede3b8e6c4b663
alumno <alumno@Lab1504-24>
2015-06-28 11:01:10 -0500

    otro cambio, nueva clase

---
+++ src/main/java/com/tecsup/spring/NewClass1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tecsup.spring;
+
+/**
+ *
+ * @author Enrique Palacios
+ */
+public class NewClass1 {
+    public static void main(String[] args) {
+        System.out.println("mensaje");
+    }
+        
+
+}
